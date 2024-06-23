48564eb20c03078287c668014cb102413b9c0c1f
Improver2108 <yashrawat2108@gmail.com>
2024-06-23 23:51:19 +0530

    "Added imports, state, and calendar component to UpcomingPage; replaced static heading with dynamic content and button"

---
+++ src/app/tasks/calender.tsx
@@ -0,0 +1,8 @@
+"use client";
+import "~/styles/calender.css";
+import { useEffect } from "react";
+import Calendar from "react-calendar";
+
+export default function Calender() {
+  return <Calendar />;
+}
--- src/app/tasks/upcoming/page.tsx
+++ src/app/tasks/upcoming/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import Image from "next/image";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Calender from "../calender";
+import { useState } from "react";
+
 export default function UpcomingPage() {
+  const date = new Date();
+  const [calendarVisible, setCalendarVisible] = useState(false);
   return (
     <>
-      <h1>This is UpcomingPage</h1>
+      <h1 className="text-4xl font-bold">Upcoming</h1>
+      <button
+        className="flex items-center gap-1"
+        onClick={() => setCalendarVisible(!calendarVisible)}
+      >
+        {`${date.toLocaleDateString("default", { month: "short" })} ${date.getFullYear()}`}
+        <RiArrowDropDownLine className="text-xl" />
+      </button>
+      {calendarVisible && (
+        <div className="max-w-[20em]">
+          <Calender />
+        </div>
+      )}
+
+      <div className="my-5 flex flex-col gap-5"></div>
     </>
   );
 }
