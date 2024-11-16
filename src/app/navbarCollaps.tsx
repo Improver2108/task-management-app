cff1662f95a55687709a7a0ca852e5b6b7f39b4b
Improver2108 <yashrawat2108@gmail.com>
2024-11-16 16:18:48 +0530

    "Added @radix-ui/react-separator and @radix-ui/react-slot dependencies, updated pnpm-lock.yaml, and modified navbarCollaps.tsx to include new imports and UI components"

--- src/app/navbarCollaps.tsx
+++ src/app/navbarCollaps.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
+import { Button } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -17,6 +20,8 @@ type TNavbarCollapse = {
   children: React.ReactNode;
 };
 
+const navLinks = ["Features", "For Teams", "Resources"];
+
 export function NavbarCollapse() {
   const [isHamburgerClicked, setIsHamburgerClicked] = useState<boolean>(false);
   return (
@@ -26,16 +31,38 @@ export function NavbarCollapse() {
           {isHamburgerClicked ? <IoMdClose /> : <GiHamburgerMenu />}
         </button>
       </SheetTrigger>
-      <SheetContent side="top" className="absolute top-[6.25rem]">
-        <ul className="space-y-4 p-4">
-          {[...Array<null>(5)].map((_, index) => (
-            <li key={index}>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          ))}
-        </ul>
+      <SheetContent side="top" className="absolute top-[6.25rem] px-3 py-8">
+        <div className="space-y-4 px-4">
+          <ul className="space-y-4">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <Button
+                  variant="ghost"
+                  className="w-full justify-start px-4 text-lg text-gray-800 hover:bg-gray-200"
+                  asChild
+                >
+                  <Link href="/" className="py-6">
+                    {link}
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+          <div className="px-4">
+            <Separator />
+          </div>
+          <div className="grid w-full grid-cols-2 gap-4 px-4">
+            <Button className="bg-gray-300 py-6 text-lg font-semibold text-black hover:bg-gray-400">
+              Sign In
+            </Button>
+            <Button
+              variant="destructive"
+              className="py-6 text-lg font-semibold"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
